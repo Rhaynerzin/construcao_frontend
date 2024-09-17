310c906ba5ed653b26660c2f235582a53a122ef4
Rhaynerzin <rhayner.mendonca@iesb.edu.br>
2024-09-16 21:41:36 -0300

    Lista de Usuários

--- listagem-usuarios/src/app/page.js
+++ listagem-usuarios/src/app/page.js
@@ -1,95 +1,41 @@
-import Image from "next/image";
-import styles from "./page.module.css";
+import { useState, useEffect } from 'react';
+import axios from 'axios';
+import { Card, Row, Col, Container } from 'react-bootstrap';
+import Link from 'next/link';
 
 export default function Home() {
-  return (
-    <div className={styles.page}>
-      <main className={styles.main}>
-        <Image
-          className={styles.logo}
-          src="https://nextjs.org/icons/next.svg"
-          alt="Next.js logo"
-          width={180}
-          height={38}
-          priority
-        />
-        <ol>
-          <li>
-            Get started by editing <code>src/app/page.js</code>.
-          </li>
-          <li>Save and see your changes instantly.</li>
-        </ol>
+  const [users, setUsers] = useState([]);
 
-        <div className={styles.ctas}>
-          <a
-            className={styles.primary}
-            href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            <Image
-              className={styles.logo}
-              src="https://nextjs.org/icons/vercel.svg"
-              alt="Vercel logomark"
-              width={20}
-              height={20}
-            />
-            Deploy now
-          </a>
-          <a
-            href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-            className={styles.secondary}
-          >
-            Read our docs
-          </a>
-        </div>
-      </main>
-      <footer className={styles.footer}>
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/file.svg"
-            alt="File icon"
-            width={16}
-            height={16}
-          />
-          Learn
-        </a>
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/window.svg"
-            alt="Window icon"
-            width={16}
-            height={16}
-          />
-          Examples
-        </a>
-        <a
-          href="https://nextjs.org?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <Image
-            aria-hidden
-            src="https://nextjs.org/icons/globe.svg"
-            alt="Globe icon"
-            width={16}
-            height={16}
-          />
-          Go to nextjs.org →
-        </a>
-      </footer>
-    </div>
+  useEffect(() => {
+    // Fazendo a requisição para pegar a lista de usuários
+    axios.get('https://dummyjson.com/users')
+      .then(response => {
+        setUsers(response.data.users);
+      })
+      .catch(error => {
+        console.error('Erro ao buscar usuários:', error);
+      });
+  }, []);
+
+  return (
+    <Container>
+      <h1 className="my-4">Lista de Usuários</h1>
+      <Row>
+        {users.map(user => (
+          <Col key={user.id} sm={12} md={6} lg={4}>
+            <Card className="mb-4">
+              <Card.Img variant="top" src={user.image} alt={`${user.firstName} ${user.lastName}`} />
+              <Card.Body>
+                <Card.Title>{user.firstName} {user.lastName}</Card.Title>
+                <Card.Text>Idade: {user.age}</Card.Text>
+                <Link href={`/user/${user.id}`} passHref>
+                  <Card.Link>Ver detalhes</Card.Link>
+                </Link>
+              </Card.Body>
+            </Card>
+          </Col>
+        ))}
+      </Row>
+    </Container>
   );
 }
