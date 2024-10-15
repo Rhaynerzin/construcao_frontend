5fe84e1f02ad9de93374f1e8bdb2fb11e0ee0665
Rhaynerzin <rhayner.mendonca@iesb.edu.br>
2024-10-15 20:31:50 -0300

    Colocando validação com yup no cadastro

---
+++ 07-crud/src/app/formularios/cadastro/page.js
@@ -0,0 +1,79 @@
+'use client'
+
+import Pagina from '@/components/Pagina'
+import { Formik } from 'formik'
+import { Button, Col, Form, Row } from 'react-bootstrap'
+import { FaCheck, FaTrash } from 'react-icons/fa'
+import * as Yup from "yup"
+
+export default function CadastroPage() {
+
+  function cadastrar(dados) {
+    console.log(dados)
+  }
+
+  const initialValues = {
+    nome: '',
+    sobrenome: ''
+  }
+
+  const validationSchema = Yup.object().shape({
+    nome: Yup.string().required("O campo nome é obrigatório!"),
+    sobrenome: Yup.string().required("O campo sobrenome é obrigatório")
+  })
+
+  return (
+    <Pagina titulo={"Cadastro de Aluno"}>
+      <Formik
+        initialValues={initialValues}
+        validationSchema={validationSchema}
+        onSubmit={cadastrar}
+      >
+        {({ values, errors, touched, handleBlur, handleSubmit, handleReset, handleChange }) => (
+          <Form onSubmit={handleSubmit}>
+
+            <Row className='mb-2'>
+              <Form.Group as={Col}>
+                <Form.Label>Nome:</Form.Label>
+                <Form.Control
+                  name='nome'
+                  type='text'
+                  value={values.nome}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isValid={touched.nome && !errors.nome}
+                  isInvalid={touched.nome && !!errors.nome}
+                />
+                <Form.Control.Feedback type='invalid'>{errors.nome}</Form.Control.Feedback>
+              </Form.Group>
+
+              <Form.Group as={Col}>
+                <Form.Label>Sobrenome:</Form.Label>
+                <Form.Control
+                  name='sobrenome'
+                  type='text'
+                  value={values.sobrenome}
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  isValid={touched.sobrenome && !errors.sobrenome}
+                  isInvalid={touched.sobrenome && !!errors.sobrenome}
+                />
+                <Form.Control.Feedback type='invalid'>{errors.sobrenome}</Form.Control.Feedback>
+              </Form.Group>
+            </Row>
+
+            <Form.Group className='text-end'>
+              <Button onClick={handleReset} className='me-2'><FaTrash /> Limpar</Button>
+              <Button type='submit' variant='success'><FaCheck /> Enviar</Button>
+            </Form.Group>
+
+
+          </Form>
+        )}
+      </Formik>
+
+
+
+    </Pagina>
+  )
+}
