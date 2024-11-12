9442ffc6863bf6bb350c706bac6c4a74efbea1f6
Rhaynerzin <rhayner.mendonca@iesb.edu.br>
2024-11-12 00:11:58 -0300

    Projeto Final

--- 09-meu-projeto-iesb/src/app/page.js
+++ 09-meu-projeto-iesb/src/app/page.js
@@ -1,10 +1,62 @@
 'use client'
 
 import Pagina from '@/components/Pagina'
+import { Button, Card, Col, Row } from 'react-bootstrap'
 
-export default function HomePage () {
+export default function HomePage() {
+
+  const veiculos = JSON.parse(localStorage.getItem("veiculos")) || []
+  const vendedores = JSON.parse(localStorage.getItem("vendedores")) || []
+  const fornecedores = JSON.parse(localStorage.getItem("fornecedores")) || []
+  const clientes = JSON.parse(localStorage.getItem("clientes")) || []
+  const vendas = JSON.parse(localStorage.getItem("vendas")) || []
+  
+  const lista = [
+    {
+      nome: "Veiculos",
+      imagem: "https://mrveiculos.com/media/img/content/bgs/bg_1692913012.jpg", quantidade: veiculos.length,
+      link: "/veiculos"
+    },
+    {
+      nome: "Vendedores",
+      imagem: "https://maximatech.com.br/wp-content/uploads/2024/07/Gestao-de-alta-demanda-como-preparar-a-equipe-de-vendedores-V2.jpg", quantidade: vendedores.length,
+      link: "/vendedores"
+    },
+    {
+      nome: "Fornecedores",
+      imagem: "https://gestao-de-fornecedores.portaliso.com/wp-content/uploads/2022/11/tipos-de-fornecedores-300x200.jpg", quantidade: fornecedores.length,
+      link: "/fornecedores"
+    },
+    {
+      nome: "Clientes",
+      imagem: "https://st.depositphotos.com/2309453/4248/i/450/depositphotos_42483789-stock-photo-smiling-young-man-shaking-hands.jpg", quantidade: clientes.length,
+      link: "/clientes"
+    },
+    {
+      nome: "Vendas",
+      imagem: "https://st2.depositphotos.com/1003593/5365/i/450/depositphotos_53656519-stock-photo-sales-growth-graph.jpg", quantidade: vendas.length,
+      link: "/vendas"
+    },
+  ]
+
   return (
-  <Pagina titulo={""}>
+    <Pagina>
+      <Row md={4}>
+        {lista.map((item, index) => (
+          <Col className='py-2' key={item.id || index}>
+            <Card className="bg-primary text-dark" style={{ height: '100%' }}>
+              <Card.Img src={item.imagem} style={{ height: '100%' }} />
+              <Card.Body>
+                <Card.Title>{item.nome}</Card.Title>
+                Cadastrados: {item.quantidade}
+              </Card.Body>
+              <Card.Footer className='text-end'>
+                <Button href={item.link} variant="light">Ver Lista</Button>
+              </Card.Footer>
+            </Card>
+          </Col>
+        ))}
+      </Row>
     </Pagina>
   )
 }
--- 09-meu-projeto-iesb/src/components/Pagina.js
+++ 09-meu-projeto-iesb/src/components/Pagina.js
@@ -1,9 +1,9 @@
 'use client'
 
 import { Container, Nav, Navbar } from "react-bootstrap"
-import { FaCar, FaListAlt, FaPlusCircle } from "react-icons/fa"
+import { FaCarAlt, FaClipboardList, FaUserFriends, FaUserTie, FaShoppingCart, FaStore } from "react-icons/fa"
 
-export default function Pagina({ titulo, children }) {
+export default function Pagina({ children }) {
 
   return (
     <>
@@ -11,35 +11,35 @@ export default function Pagina({ titulo, children }) {
       <Navbar bg="primary" variant="light" expand="lg">
         <Container>
           <Navbar.Brand href="/">
-            <FaCar className="me-2" />
+            <FaCarAlt className="me-2" />
             Paiva's Car
           </Navbar.Brand>
           <Navbar.Toggle aria-controls="navbar-nav" />
           <Navbar.Collapse id="navbar-nav">
             <Nav className="ms-auto">
               <Nav.Link href="/veiculos">
-                <FaListAlt className="me-2" />
+                <FaClipboardList className="me-2" />
                 Lista de Veículos
               </Nav.Link>
               <Nav.Link href="/veiculos/form">
-                <FaPlusCircle className="me-2" />
+                <FaCarAlt className="me-2" />
                 Novo Veículo
               </Nav.Link>
               <Nav.Link href="/vendedores">
-                <FaListAlt className="me-2" />
+                <FaUserTie className="me-2" />
                 Vendedores
               </Nav.Link>
+              <Nav.Link href="/fornecedores">
+                <FaStore className="me-2" />
+                Fornecedores
+              </Nav.Link>
               <Nav.Link href="/clientes">
-                <FaListAlt className="me-2" />
+                <FaUserFriends className="me-2" />
                 Clientes
               </Nav.Link>
               <Nav.Link href="/vendas">
-                <FaListAlt className="me-2" />
+                <FaShoppingCart className="me-2" />
                 Vendas
-              </Nav.Link>
-              <Nav.Link href="">
-                <FaListAlt className="me-2" />
-                Dashboard
               </Nav.Link>
             </Nav>
           </Navbar.Collapse>
